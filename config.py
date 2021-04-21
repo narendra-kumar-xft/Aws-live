230ddc8fc7c864ff9772758ebc0962e3cb10fce0
Narendra Kumar-XFT <72868474+narendra-kumar-xft@users.noreply.github.com>
2021-04-21 15:32:23 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "trnkolpoptknp9.cmuq4uiatswr.ap-southeast-1.rds.amazonaws.com"
+customhost = "sr8dewpl77obvf.cmuq4uiatswr.ap-southeast-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin1234"
 customdb = "spartans"
